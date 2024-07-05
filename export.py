3faa1cc8beaf649c5498cbd0b3d9c5f80e2ee239
AmaseCocoa <amase.cocoa@gmail.com>
2024-07-05 23:45:19 +0900

    deleted

--- export.py
+++ export.py
@@ -1,16 +1,12 @@
 import datetime
 import os
 import random
-
 import yaml
 from jinja2 import Environment, FileSystemLoader
-
 from src import static
-
 template_dir = 'templates'
 i18n_dir = 'i18n'
 output_base_dir = 'dist'
-
 env = Environment(loader=FileSystemLoader(template_dir))
 randomComments = [
     "Why don't programmers like nature? It has too many bugs.",
@@ -18,7 +14,6 @@ randomComments = [
     "Why do programmers prefer dark mode? Because light attracts bugs.",
     "If you find this comment, tell Bob he owes us coffee.",
 ]
-
 def buildPath(path, isIndex: bool, locale: str):
     if isIndex:
         return path
@@ -26,37 +21,24 @@ def buildPath(path, isIndex: bool, locale: str):
         return "/" + path
     else:
         return "/" + locale + path
-
-# ローカライズデータの読み込み
 def load_translations(locale):
     with open(os.path.join(i18n_dir, f'{locale}.yml'), 'r', encoding='utf-8') as file:
         return yaml.safe_load(file)
-
-# テンプレートのレンダリングと保存
 def render_and_save_template(template_name, translations, output_dir):
     template = env.get_template(template_name)
     rendered_content = template.render(**translations, buildpath=buildPath)
-    
-    # 出力ディレクトリの作成
     os.makedirs(output_dir, exist_ok=True)
-    
-    # ファイルの保存
     output_file = os.path.join(output_dir, 'index.html')
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(rendered_content)
-
-# メイン処理
 def main():
     locales = ['en', 'ja']
     
     for locale in locales:
         translations = load_translations(locale)
-        
         templates = [f for f in os.listdir(template_dir) if f.endswith('.html') and f != 'base.html']
-        
         for template_file in templates:
             template_name = os.path.splitext(template_file)[0]
-            
             if template_file == 'index.html':
                 if locale == 'en':
                     translations["randomMsg"] = randomComments[1]
@@ -85,7 +67,6 @@ def main():
                 translations["isIndex"] = False
             translations["updatedAt"] = datetime.datetime.now(datetime.UTC).strftime('%Y/%m/%d %H:%M:%S (UTC)')
             render_and_save_template(template_file, translations, output_dir)
-
 if __name__ == '__main__':
     static.copy()
     main()
