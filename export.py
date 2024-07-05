6c752a765a5605c83a68d104adc245a035d411d4
AmaseCocoa <amase.cocoa@gmail.com>
2024-07-05 23:45:57 +0900

    関数名変更

--- export.py
+++ export.py
@@ -1,9 +1,12 @@
 import datetime
 import os
 import random
+
 import yaml
 from jinja2 import Environment, FileSystemLoader
+
 from src import static
+
 template_dir = 'templates'
 i18n_dir = 'i18n'
 output_base_dir = 'dist'
@@ -14,6 +17,7 @@ randomComments = [
     "Why do programmers prefer dark mode? Because light attracts bugs.",
     "If you find this comment, tell Bob he owes us coffee.",
 ]
+
 def buildPath(path, isIndex: bool, locale: str):
     if isIndex:
         return path
@@ -21,19 +25,21 @@ def buildPath(path, isIndex: bool, locale: str):
         return "/" + path
     else:
         return "/" + locale + path
+
 def load_translations(locale):
     with open(os.path.join(i18n_dir, f'{locale}.yml'), 'r', encoding='utf-8') as file:
         return yaml.safe_load(file)
-def render_and_save_template(template_name, translations, output_dir):
+
+def render(template_name, translations, output_dir):
     template = env.get_template(template_name)
     rendered_content = template.render(**translations, buildpath=buildPath)
     os.makedirs(output_dir, exist_ok=True)
     output_file = os.path.join(output_dir, 'index.html')
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(rendered_content)
+
 def main():
     locales = ['en', 'ja']
-    
     for locale in locales:
         translations = load_translations(locale)
         templates = [f for f in os.listdir(template_dir) if f.endswith('.html') and f != 'base.html']
@@ -66,7 +72,7 @@ def main():
                     output_dir = os.path.join(output_base_dir, locale, template_name)
                 translations["isIndex"] = False
             translations["updatedAt"] = datetime.datetime.now(datetime.UTC).strftime('%Y/%m/%d %H:%M:%S (UTC)')
-            render_and_save_template(template_file, translations, output_dir)
+            render(template_file, translations, output_dir)
 if __name__ == '__main__':
     static.copy()
     main()
